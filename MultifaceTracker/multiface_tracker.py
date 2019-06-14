2208b745ba3d94ec8150b89eb07fd67384c21fa7
Emil Winebrand <emilio1234@gmail.com>
2019-06-14 09:33:07 +0300

    Now running good on two faces

--- MultifaceTracker/multiface_tracker.py
+++ MultifaceTracker/multiface_tracker.py
@@ -13,6 +13,11 @@ from ipdb import set_trace as dbg
 def debug_msg(string):
     print(string)
 
+def normalize_desc(desc):
+    desc = np.array(desc)
+    desc /= np.linalg.norm(desc) #performane improves with normalization
+    return desc.tolist() #save lists to allow json serialization
+
 
 class NameGenerator():
     def __init__(self,filename):
@@ -29,7 +34,7 @@ class NameGenerator():
         return current_name
 
 class IDMatchingManager():
-    def __init__(self,classifier_path,th_similar,th_symmetry,max_desc_len):
+    def __init__(self,classifier_path,th_similar,th_symmetry,max_desc_len,yaw_th):
         self.history_ids = {}
         self.first_appearance = {}
         self.revese_matches = bidict()
@@ -38,22 +43,27 @@ class IDMatchingManager():
         self.max_desc_len = max_desc_len
         self.cycle_counter = 0
         self.name_gen = NameGenerator("./dist.male.first")
+        self.yaw_th = yaw_th
 
         with open(classifier_path, 'rb') as fd:
             self.classifier_MLP = pickle.load(fd)
-    
+   
+    def append_id(self,new_id,desc):
+        pass
+
     def compare_descriptors(self,desc1,desc2,th_sym):
-        best_match = -1.0
-        for d1 in desc1:
-            for d2 in desc2:
+        corr_mtx = np.full((len(desc1),len(desc2)),-1.0,dtype=float)
+        for id1,d1 in enumerate(desc1):
+            for id2,d2 in enumerate(desc2):
                 test_vec = np.concatenate((d1,d2))
                 match_prob1 = self.classifier_MLP.predict_proba([test_vec])[:,1]
                 match_prob2 = self.classifier_MLP.predict_proba([np.fft.fftshift(test_vec)])[:,1]
                 if np.abs(match_prob1-match_prob2) > th_sym:
                     tracked_match = -1.0 ## No decision
                 tracked_match = (match_prob1+match_prob2)*0.5
-                best_match = tracked_match if tracked_match>best_match else best_match
-        return best_match
+                corr_mtx[id1,id2] = tracked_match 
+                #best_match = tracked_match if tracked_match>best_match else best_match
+        return corr_mtx
 
     def preload_ids(self,json_file):
         with open(json_file, 'r') as fp:
@@ -64,7 +74,9 @@ class IDMatchingManager():
         with open(json_file, 'w') as fp:
             json.dump(temp_ids,fp,indent = 4)
 
-    def update_matches(self,tracked_ids):
+    def update_matches(self,pcn_faces):
+        tracked_ids = {pcn_det.id:normalize_desc(pcn_det.descriptor) for pcn_det in pcn_faces}
+        tracked_yaw = {pcn_det.id:pcn_det.yaw for pcn_det in pcn_faces}
 
         hist_keys = list(self.history_ids.keys())
         hist_desc = list(self.history_ids.values())
@@ -76,44 +88,65 @@ class IDMatchingManager():
         for idx_hist, desc_hist in enumerate(hist_desc):
             for idx_tracked,desc_tracked in enumerate(tracked_desc):
                 match_prob = self.compare_descriptors(desc_hist,[desc_tracked],self.th_symmetry)
-                corr_mtx[idx_hist,idx_tracked] = match_prob
+                corr_mtx[idx_hist,idx_tracked] = np.max(match_prob)
         row_ind, col_ind = linear_sum_assignment(-corr_mtx)
 
-        if self.cycle_counter == 29:
-            disc1 = self.history_ids["JAMES"]
-            disc2 = self.history_ids["MICHAEL"]
-            test_match = self.compare_descriptors(disc1,disc2,0.02)
-            dbg()
-       
+        #if self.cycle_counter == 99:
+        #    dbg()
+
+        corr_dict = {} 
+        for ih1 in range(len(hist_keys)):
+            for ih2 in range(ih1,len(hist_keys)):
+                h1 = hist_keys[ih1]
+                h2 = hist_keys[ih2]
+                if h1 == h2:
+                    continue
+                match_prob = self.compare_descriptors(self.history_ids[h1],self.history_ids[h2],self.th_symmetry)
+                corr_dict[(h1,h2)] = np.max(match_prob)
+        debug_msg("{0}: corr_dict = {1}".format(self.cycle_counter,corr_dict))
+        #debug_msg("{0}: JOHN * JAMES = {1}".format(self.cycle_counter,match_prob))
+
+
         # Generate tracked faces
         revese_matches = bidict()
         undecided = []
         key_pairs_for_merge = []
         for r,c in zip(row_ind,col_ind):
-            if corr_mtx[r,c] > self.th_similar: 
+            if corr_mtx[r,c] > self.th_similar and \
+                    tracked_yaw[tracked_keys[c]] < self.yaw_th: 
                 ## In case of duplication we want to merge history
-                if tracked_keys[c] in self.revese_matches and \
-                        hist_keys[r] != self.revese_matches[tracked_keys[c]]: 
-                    key_pairs_for_merge.append((hist_keys[r],self.revese_matches[tracked_keys[c]]))
+                #if tracked_keys[c] in self.revese_matches and \
+                #        hist_keys[r] != self.revese_matches[tracked_keys[c]]: 
+                #    key_pairs_for_merge.append((hist_keys[r],self.revese_matches[tracked_keys[c]]))
                 revese_matches[tracked_keys[c]] = hist_keys[r]
             else:
                 debug_msg("{0}: Undecided {1:.2f}".format(self.cycle_counter,corr_mtx[r,c]))
                 undecided.append((r,c))
 
         ## Assign tracked faces since there are mote face than db
-        ##TODO: Make sure unassigned are actually new numbers!!
         unassigned = np.delete(np.arange(0,len(tracked_keys),1),col_ind).tolist()
         undecided_c = [u[1] for u in undecided]
 
         for c in unassigned + undecided_c:
-            if tracked_keys[c] in self.revese_matches: ##TODO:consider hgistory
-                ##TODO: What happens if the hist is already assigned!!?
-                self.history_ids[self.revese_matches[tracked_keys[c]]].append(tracked_desc[c]) #found a tracked desc of the face
-                revese_matches[tracked_keys[c]] = self.revese_matches[tracked_keys[c]] #maintain reverse matching
+            if tracked_keys[c] in self.revese_matches: 
+
+                ##Checks if the history is already assigned
+                if self.revese_matches[tracked_keys[c]] in revese_matches.inverse:
+                    continue
+
+                revese_matches[tracked_keys[c]] = self.revese_matches[tracked_keys[c]] #continuous reverse matching
+
+                #if good yaw then we might have a new descriptor for existing face
+                if tracked_yaw[tracked_keys[c]] < self.yaw_th:
+                    debug_msg("{0}: New descriptor added to {1} from id {2}".format(self.cycle_counter,self.revese_matches[tracked_keys[c]],tracked_keys[c]))
+                    self.history_ids[self.revese_matches[tracked_keys[c]]].append(tracked_desc[c]) #found a tracked desc of the face
+
             else: ## A totally new face
-                assigned_key = self.name_gen.get_full_name()
-                self.history_ids[assigned_key] = deque([tracked_desc[c]],self.max_desc_len)
-                revese_matches[tracked_keys[c]] = assigned_key
+                ## Assign new key only if low enough yaw
+                if tracked_yaw[tracked_keys[c]] < self.yaw_th:
+                    assigned_key = self.name_gen.get_full_name()
+                    self.history_ids[assigned_key] = deque([tracked_desc[c]],self.max_desc_len)
+                    revese_matches[tracked_keys[c]] = assigned_key
 
         ## Perform merges 
         for a_key,b_key in key_pairs_for_merge:
@@ -156,22 +189,16 @@ class IDMatchingManager():
 
 
 class MultifaceTracker():
-    def __init__(self,classifier_path,th_similar,th_symmetry,max_desc_len,*args):
-        self.ids_manager = IDMatchingManager(classifier_path,th_similar,th_symmetry,max_desc_len)
+    def __init__(self,classifier_path,th_similar,th_symmetry,max_desc_len,yaw_th, *args):
+        self.ids_manager = IDMatchingManager(classifier_path,th_similar,th_symmetry,max_desc_len,yaw_th)
         self.pcn_detector = PCN(*args)
 
-
-    def _normalize_desc(self,desc):
-        desc = np.array(desc)
-        desc /= np.linalg.norm(desc) #performane improves with normalization
-        return desc.tolist() #save lists to allow json serialization
 
     def track_image(self,img):
         pcn_faces = self.pcn_detector.DetectAndTrack(img)
 
         if self.pcn_detector.CheckTrackingPeriod()==self.pcn_detector.track_period:
-            tracked_ids = {pcn_det.id:self._normalize_desc(pcn_det.descriptor) for pcn_det in pcn_faces}
-            self.ids_manager.update_matches(tracked_ids)
+            self.ids_manager.update_matches(pcn_faces)
 
         return pcn_faces
 
@@ -194,15 +221,15 @@ if __name__=="__main__":
 
     mface = MultifaceTracker(
             classifier_path,
-            0.7,0.02,50,
+            0.7,0.02,50,0.5,
             detection_model_path,pcn1_proto,pcn2_proto,pcn3_proto,
             tracking_model_path,tracking_proto, 
             embed_model_path, embed_proto,
-            40,1.45,0.5,0.5,0.98,30,0.9,1)
+            40,1.45,0.3,0.3,0.5,30,0.9,1)
     #if os.path.isfile("./tracking.json"):
     #    mface.ids_manager.preload_ids("./tracking.json")
 
-    cap = cv2.VideoCapture("./test_tracked.mp4")
+    cap = cv2.VideoCapture("./test_tracked2.mp4")
     #cap = cv2.VideoCapture(0)
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -214,15 +241,14 @@ if __name__=="__main__":
         ret, img = cap.read()
         if img is None or img.shape[0] == 0:
             break
-        #writer.write(img)
-
+        writer.write(img)
         
         try:
             faces = mface.track_image(img) 
         except:
-            pass
-            #writer.release()
-            #exit()
+            writer.release()
+            exit()
+        #faces = mface.track_image(img) 
 
         for face in faces:
             name = mface.ids_manager.check_match(face.id)
@@ -234,7 +260,7 @@ if __name__=="__main__":
         cv2.putText(img,str(mface.ids_manager.cycle_counter),(10,80), cv2.FONT_HERSHEY_SIMPLEX, 3,(0,255,0),3,cv2.LINE_AA)
         cv2.imshow('window', img)
         mface.pcn_detector.CheckTrackingPeriod()
-        writer.write(img)
+        #writer.write(img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     mface.ids_manager.save_ids("tracking.json")
